ab00881628abb646631c7b692a47ccbe9bff24dd
takuya <cdwbh564@yahoo.co.jp>
2025-04-10 22:13:10 +0900

    feat:controllerでcreateメソッドを実装し、シャッフル機能を追加
    - 事前に作成していたシャッフル機能のロジックを、index.phpから移行

--- src/index.php
+++ src/index.php
@@ -1,4 +1,8 @@
 <?php
+
+// 現在は不要のファイル　全てShuffleController.phpのcreateメソッドへ移行
+
+
     $errors = [];
     $groups = [];
 
@@ -28,37 +32,5 @@
             array_push($groups[0], $extra);
         }
     }
-?>
 
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>シャッフルランチ</title>
-</head>
-
-<body>
-    <h1>
-        <a href="./index.php">シャッフルランチ</a>
-    </h1>
-    <a href="./employees.php">社員を登録する</a>
-
-    <form action="./index.php" method="post">
-        <button type="submit">シャッフルする</button>
-    </form>
-
-    <!-- グループ毎に社員名を出力 -->
-    <?php foreach ($groups as $i => $group) : ?>
-        <h3>
-            グループ<?php echo ($i + 1) ; ?>
-        </h3>
-        <?php foreach ($group as $employee) : ?>
-            <?php echo $employee['name']; ?>
-        <?php endforeach; ?>
-
-    <?php endforeach; ?>
-
-</body>
-
-</html>
+    include __DIR__ . '/views/index.php';
