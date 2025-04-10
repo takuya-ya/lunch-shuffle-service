e720410dc26e9e8a44b1fa80c3dbb44712dfd671
takuya <cdwbh564@yahoo.co.jp>
2025-04-10 22:11:13 +0900

    feat:ルーティングを設定

---
+++ src/Application.php
@@ -0,0 +1,53 @@
+<?php
+
+// ルート設定をRouterで行う
+require_once __DIR__ . '/core/Router.php';
+require_once __DIR__ . '/contoroller/ShuffleController.php';
+
+// 初期設定を行う？
+class Application
+{
+    public $router;
+
+    public function __construct()
+    {
+        //urlとcontroller,actionの対応表を渡し、Routerがそれを管理する
+            // 引数:url => [controller,action]
+        $this->router = new Router($this->registerRoutes());
+    }
+
+    public function run()
+    {
+        // 名前解決　resolveの引数であるパスと、constで設定したコントローラー（アクション？）を紐づけ
+            // 引数：パス
+            // 返り値 $params = [controller => 〇〇, action => 〇〇];
+        $params = $this->router->resolve($this->getPathInfo());
+        $controller = $params['controller'];
+        $action = $params['action'];
+        $this->runAction($controller, $action);
+    }
+
+    // controllerの動的生成と実行
+    private function runAction($controllerName, $action)
+    {
+        $controllerClass = ucfirst($controllerName) . 'Controller';
+        $controller = new $controllerClass();
+        $controller->run($action);
+    }
+
+    // 配列で
+    private function registerRoutes()
+
+    {
+        return [
+            '/' => ['controller' => 'shuffle', 'action' => 'index'],
+            '/shuffle' => ['controller' => 'shuffle', 'action' => 'create']
+        ];
+    }
+
+    // ブラウザがリクエストするパスの情報を取得
+    private function getPathInfo()
+    {
+        return $_SERVER['REQUEST_URI'];
+    }
+}
---
+++ src/contoroller/ShuffleController.php
@@ -0,0 +1,64 @@
+<?php
+
+class ShuffleController
+{
+    // $actionに'index'
+    public function run($action)
+    {
+        $this->$action();
+    }
+
+    private function index()
+    {
+        $groups = [];
+
+        // DBに接続
+        $mysqli = new mysqli('db', 'test_user', 'pass', 'test_database');
+
+        // 接続エラーのバリデーション
+            // connect_error 接続エラーの内容（文字列）を返すプロパティ
+        if ($mysqli->connect_error) {
+            // PHP組み込み例外クラス:「実行時に予期せぬ問題が発生」を表示
+            throw new RuntimeException('mysqli接続エラー: ' . $mysqli->connect_error);
+        }
+
+        include __DIR__ . '/../views/index.php';
+    }
+
+
+    private function create()
+    {
+
+        $groups = [];
+
+        // DBに接続
+        $mysqli = new mysqli('db', 'test_user', 'pass', 'test_database');
+
+        // 接続エラーのバリデーション
+            // connect_error 接続エラーの内容（文字列）を返すプロパティ
+        if ($mysqli->connect_error) {
+            // PHP組み込み例外クラス:「実行時に予期せぬ問題が発生」を表示
+            throw new RuntimeException('mysqli接続エラー: ' . $mysqli->connect_error);
+        }
+
+        $result = $mysqli->query('SELECT name FROM employees;');
+        $employees = $result->fetch_all(MYSQLI_ASSOC);
+        shuffle($employees);
+
+        // 2名のグループで余る人の有無を確認
+        $cnt = count($employees);
+
+        if ($cnt % 2 === 0 ) {
+            $groups =  array_chunk($employees, 2);
+        } else {
+            // var_dump($groups);
+            $extra = array_pop($employees);
+            $groups = array_chunk($employees, 2);
+            array_push($groups[0], $extra);
+        }
+
+        include __DIR__ . '/../views/index.php';
+
+    }
+
+}
