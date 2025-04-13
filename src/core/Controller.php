5a3c79a3aba315f2b5ed877414a6dca48a1d6cf9
takuya <cdwbh564@yahoo.co.jp>
2025-04-13 20:17:14 +0900

    feat:未定義ルートへのアクセス時に404ページを表示する処理を追加
    - HTTPステータスコード404を返す処理を実装
    - エラー種別を明確にする為、専用例外クラス`HttpNotFoundException`を作成

--- src/core/Controller.php
+++ src/core/Controller.php
@@ -5,6 +5,11 @@ class Controller
     // $actionに'index'
     public function run($action)
     {
+        // $thisクラスにおける$actionメソッドの有無を確認
+        if (!method_exists($this, $action)) {
+            throw new HttpNotFoundException();
+        }
+
         $this->$action();
     }
 }
